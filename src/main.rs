d71e1df2069d0f72faebba6bb895b8e3a6f26727
gleb <12bny23gleb@Gmail.com>
2020-08-16 17:28:24 +0300

    saving query & filename in self variables

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,10 @@ use std::env;
 fn main() {
 	// Collect all arguments in one Vector
 	let args: Vec<String> = env::args().collect();
-	println!("{:?}", args);
+	
+	let query = &args[1];
+	let filename = &args[2];
+
+	println!("Searching for {}", query);
+	println!("In file {}", filename);
 }
